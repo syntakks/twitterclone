1c69f82dd644c0a290af52c9014669080b457351
syntakks <syntakks@gmail.com>
2021-05-17 10:54:42 -0500

    Fixed issues with tweet image/ FeedView

---
+++ twitterclone/Views/FeedView.swift
@@ -0,0 +1,40 @@
+//
+//  FeedView.swift
+//  twitterclone
+//
+//  Created by Stephen Wall on 5/17/21.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+      ZStack(alignment: .bottomTrailing) {
+        
+        ScrollView {
+          
+        }
+        
+        Button(
+          action: {},
+          label: {
+            Image("tweet")
+              .resizable()
+              .renderingMode(.template)
+              .frame(width: 32, height: 32)
+              .padding()
+          })
+          .background(Color.blue)
+          .foregroundColor(.white)
+          .clipShape(Circle())
+          .padding()
+        
+      }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
