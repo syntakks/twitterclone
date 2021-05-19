5035477b5faee487711e20f583e2c14255d7bf5f
Steve Wall <syntakks@gmail.com>
2021-05-18 20:35:59 -0500

    Added Firebase via SPM

--- twitterclone/App/twittercloneApp.swift
+++ twitterclone/App/twittercloneApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct twittercloneApp: App {
+  
+  init() {
+    FirebaseApp.configure()
+  }
+  
   var body: some Scene {
     WindowGroup {
       //ContentView()
